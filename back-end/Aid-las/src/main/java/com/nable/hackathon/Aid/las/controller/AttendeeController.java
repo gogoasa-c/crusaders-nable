b0ebf23fd88d93fa73a675cf9656e926024d577a
Cristian Gogoasa <gogoasac16@gmail.com>
2023-10-28 12:14:42 +0300

    Added controllers

--- back-end/Aid-las/src/main/java/com/nable/hackathon/Aid/las/controller/AttendeeController.java
+++ back-end/Aid-las/src/main/java/com/nable/hackathon/Aid/las/controller/AttendeeController.java
@@ -1,7 +1,48 @@
 package com.nable.hackathon.Aid.las.controller;
 
+import com.nable.hackathon.Aid.las.entity.Attendee;
+import com.nable.hackathon.Aid.las.service.AttendeeService;
+import jakarta.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
+@RequestMapping("/attendee")
+@Slf4j
 public class AttendeeController {
+    @Resource
+    public AttendeeService attendeeService;
+    @GetMapping("/{attendeeId}")
+    public ResponseEntity<Attendee> getAttendee(@PathVariable Integer attendeeId) {
+        log.info("Entered getAttendee with attendeeId: {}", attendeeId);
+
+        // to be modified with service method call
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<Attendee> createAttendee(@RequestBody Attendee attendee) {
+        log.info("Entered createAttendee");
+
+        // to be modified with service method call
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping("/{attendeeId}")
+    public ResponseEntity<Attendee> updateAttendee(@PathVariable Integer attendeeId, @RequestBody Attendee attendee) {
+        log.info("Entered updateAttendee with attendeeId: {} and attendee: {}", attendeeId, attendee);
+
+        // to be modified with service method call
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/{attendeeId}")
+    public ResponseEntity<Attendee> deleteAttendee(@PathVariable Integer attendeeId) {
+        log.info("Entered deleteAttendee with attendeeId: {}", attendeeId);
+
+        // to be modified with service method call
+        return ResponseEntity.noContent().build();
+    }
 }
--- back-end/Aid-las/src/main/java/com/nable/hackathon/Aid/las/controller/OrganizerController.java
+++ back-end/Aid-las/src/main/java/com/nable/hackathon/Aid/las/controller/OrganizerController.java
@@ -1,7 +1,50 @@
 package com.nable.hackathon.Aid.las.controller;
 
+import com.nable.hackathon.Aid.las.entity.Organizer;
+import com.nable.hackathon.Aid.las.service.OrganizerService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/organizer")
+@Slf4j
 public class OrganizerController {
+    public OrganizerService organizerService;
+
+    @GetMapping("/{organizerId}")
+    public ResponseEntity<Organizer> getOrganizer(@PathVariable Integer organizerId) {
+        log.info("Entered getOrganizer with organizerId: {}", organizerId);
+
+        // to be modified with service method call
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/create")
+    public ResponseEntity<Organizer> createOrganizer() {
+        log.info("Entered createOrganizer");
+
+        // to be modified with service method call
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/{organizerId}/update")
+    public ResponseEntity<Organizer> updateOrganizer(@PathVariable Integer organizerId, @RequestBody Organizer organizer) {
+        log.info("Entered updateOrganizer with organizerId: {}", organizerId);
+
+        // to be modified with service method call
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/{organizerId}/delete")
+    public ResponseEntity<Organizer> deleteOrganizer(@PathVariable Integer organizerId) {
+        log.info("Entered deleteOrganizer with organizerId: {}", organizerId);
+
+        // to be modified with service method call
+        return ResponseEntity.noContent().build();
+    }
 }
