2176f9726c4d2ae67e12e6bb294a1730c0ff8427
teabee53 <tiberiucristian2002@gmail.com>
2023-10-28 11:02:57 +0300

    did stuff

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+//import { useState } from 'react'
+
+const generateList = ()=>{
+  let content=[]
+  for(let i = 0; i < 4; i++)
+  content.push('Element '+i)
+  return content
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <>
+          <div className="List">
+            <ul>{generateList().map(row=><li>{row}</li>)}</ul>
+          </div>
+        </>
+      </div>
+    </>
   );
 }
 
