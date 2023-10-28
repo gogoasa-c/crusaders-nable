2556501300e684c5689ba8dc41731f3da3aea12f
Cristian Gogoasa <gogoasac16@gmail.com>
2023-10-28 11:56:59 +0300

    Removed @Data from classes

--- back-end/Aid-las/src/main/java/com/nable/hackathon/Aid/las/entity/Attendee.java
+++ back-end/Aid-las/src/main/java/com/nable/hackathon/Aid/las/entity/Attendee.java
@@ -1,15 +1,8 @@
 package com.nable.hackathon.Aid.las.entity;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.Inheritance;
-import jakarta.persistence.InheritanceType;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 
 @Entity
-@Data
-public class Attendee extends User{
+public class Attendee extends User {
 
 }
--- back-end/Aid-las/src/main/java/com/nable/hackathon/Aid/las/entity/Organizer.java
+++ back-end/Aid-las/src/main/java/com/nable/hackathon/Aid/las/entity/Organizer.java
@@ -1,16 +1,9 @@
 package com.nable.hackathon.Aid.las.entity;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.experimental.SuperBuilder;
 
-import java.util.List;
 
 @Entity
-@Data
 public class Organizer extends User {
 
 }
